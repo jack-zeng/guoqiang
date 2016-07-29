238ea231bd8a18ae76e49c756ba0fbbe415d9143
jack-zeng <2480399489@qq.com>
2016-07-29 11:08:26 +0800

    修改效果

--- js/culture.js
+++ js/culture.js
@@ -2,7 +2,7 @@ $(function(){
 var window_width=$(window).width();
  if(window_width>1201){
 	$(".culture ul li").mouseenter(function(){
-		$(this).css({width:580}).children('.cultureCont').animate({left:285}).end().siblings().css({width:250}).children(".cultureCont").css({left:0})
+		$(this).addClass('cutRight').siblings("li").removeClass('cutRight')
  })
 }
 })
